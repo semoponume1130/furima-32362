60ee69158a0d9fe6794d3e08b9c59b909b1227b1
semoponume1130 <goldship1130@gmail.com>
2021-05-29 21:10:53 +0900

    正常系テストコードの変更、itemsコントローラーのコメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,15 @@
-<%# class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+# class ItemsController < ApplicationController
+#  before_action :move_to_index, except: [:index, :show]
 
-  def index
-  end
+#  def index
+#  end
 
-  def new
-  end
+#  def new
+# end
 
-  def edit
-  end
+# def edit
+# end
 
-  def show
-  end
-end %>
+# def show
+# end
+#end 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,51 +7,12 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー情報の保存' do
     context '保存できる場合' do
-      it 'nicknameが必須であること' do
-      end
   
-      it 'emailが必須であること' do
-      end
-  
-      it 'emailが一意性であること' do
+      it 'ユーザー情報が正しく保存できること' do
         @user.save
-        another_user = FactoryBot.build(:user)
-        another_user.email != @user.email
-        expect(another_user).to be_valid
+        expect(@user).to be_valid
       end
   
-      it 'emailは@を含む必要があること' do
-      end
-  
-      it 'passwordが必須であること' do
-      end
-
-      it 'passwordが6文字以上での入力が必須であること' do
-      end
-  
-      it 'passwordは確認用を含めて2回入力すること' do
-      end
-  
-      it 'passwordとpassword_confirmationの値の一致が必須であること' do
-      end
-  
-      it 'ユーザー本名は、名字と名前がそれぞれ必須であること' do
-      end
-  
-      it 'ユーザー本名は、全角（漢字・ひらがな・カタカナ）での入力が必須であること' do
-      end
-  
-      it 'ユーザー本名のフリガナは、名字と名前でそれぞれ必須であること' do
-      end
-  
-      it 'ユーザー本名のフリガナは、名字と名前でそれぞれ必須であること' do
-      end
-  
-      it 'ユーザー本名のフリガナは、全角（カタカナ）での入力が必須であること' do
-      end
-  
-      it '生年月日が必須であること' do
-      end
     end
 
 
